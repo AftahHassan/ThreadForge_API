1809a7c49198a28a2803d8b766b8f17342d2dd14
hassan_aftah <aftahhassan25@gmail.com>
2026-06-24 13:07:34 +0100

    rawcontent

--- app/Http/Resources/RawContentResource.php
+++ app/Http/Resources/RawContentResource.php
@@ -7,13 +7,15 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class RawContentResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'campaign_id' => $this->campaign_id,
+            'content' => $this->content,
+            'source_type' => $this->source_type,
+            'processing_status' => $this->processing_status,
+            'error_message' => $this->error_message,
+        ];
     }
 }
