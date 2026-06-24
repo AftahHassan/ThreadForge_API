8748c0f292bea5d7c0364d99de9b630323140ab4
hassan_aftah <aftahhassan25@gmail.com>
2026-06-24 16:53:38 +0100

    afficher les postes

--- app/Http/Resources/GeneratedPostResource.php
+++ app/Http/Resources/GeneratedPostResource.php
@@ -13,6 +13,26 @@ class GeneratedPostResource extends JsonResource
             'id' => $this->id,
             'campaign_id' => $this->campaign_id,
             'raw_content_id' => $this->raw_content_id,
+
+            'campaign' => $this->whenLoaded('campaign', function () {
+                return [
+                    'id' => $this->campaign->id,
+                    'name' => $this->campaign->name,
+                    'tone' => $this->campaign->tone,
+                    'max_characters' => $this->campaign->max_characters,
+                    'max_hashtags' => $this->campaign->max_hashtags,
+                ];
+            }),
+
+            'raw_content' => $this->whenLoaded('rawContent', function () {
+                return [
+                    'id' => $this->rawContent->id,
+                    'content' => $this->rawContent->content,
+                    'source_type' => $this->rawContent->source_type,
+                    'processing_status' => $this->rawContent->processing_status,
+                ];
+            }),
+
             'hook_propose' => $this->hook_propose,
             'body_points' => $this->body_points,
             'technical_readability_score' => $this->technical_readability_score,
