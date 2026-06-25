61c271ee87bf74de0526d55bdb71e7a9fa861926
hassan_aftah <aftahhassan25@gmail.com>
2026-06-25 12:16:38 +0100

    request_chat

--- app/Http/Requests/Chat/SendMessageRequest.php
+++ app/Http/Requests/Chat/SendMessageRequest.php
@@ -2,28 +2,20 @@
 
 namespace App\Http\Requests\Chat;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class SendMessageRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'message' => ['required', 'string', 'min:3'],
+            'conversation_id' => ['nullable', 'integer', 'exists:conversations,id'],
         ];
     }
 }
